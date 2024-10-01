f8d83137b60c58ac6de91761d0109493ad881a3c
Ishant Bhatt <ishantbhatt1654@gmail.com>
2024-10-01 18:11:43 +0530

    changes: changes in register page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -13,6 +13,7 @@ function Page() {
   const [email, setEmail] = useState("");
   const [showpassword, setShowpassword] = useState(false);
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   // const [isRegistering, setIsRegistering] = useState(false);
   const [error, setError] = useState("");
   const router = useRouter();
@@ -23,6 +24,14 @@ function Page() {
     setError("");
     // if (isRegistering) {
     // Register
+    console.log(password == confirmPassword);
+    if (password !== confirmPassword) {
+      setError((prev) => ({
+        ...prev,
+        passwordMismatch: "Passwords do not match",
+      }));
+      return;
+    }
     const res = await fetch("/api/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -128,16 +137,22 @@ function Page() {
             </div>
           </div>
           <div>
-            <div class="relative">
+            <div
+              className={`relative rounded-lg ${
+                error.passwordMismatch ? "border-red-500 border" : "border-none"
+              }`}
+            >
               <input
                 type={showpassword ? "text" : "password"}
-                id="Confirmpassword"
+                id="confirmpassword"
                 className="peer p-4 w-full bg-gray-100 border-transparent rounded-lg text-sm placeholder:text-transparent focus:outline-none focus:ring-1 focus:border-orange-500 focus:ring-orange-500 disabled:opacity-50 disabled:pointer-events-none focus:pt-6 focus:pb-2 [&:not(:placeholder-shown)]:pt-6 [&:not(:placeholder-shown)]:pb-2 autofill:pt-6 autofill:pb-2"
                 placeholder=""
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
                 required
               />
               <label
-                for="Confirmpassword"
+                for="confirmpassword"
                 className="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent  origin-[0_0]  peer-disabled:opacity-50 peer-disabled:pointer-events-none peer-focus:scale-90 peer-focus:translate-x-0.5 peer-focus:-translate-y-1.5 peer-focus:text-gray-500  peer-[:not(:placeholder-shown)]:scale-90 peer-[:not(:placeholder-shown)]:translate-x-0.5 peer-[:not(:placeholder-shown)]:-translate-y-1.5 peer-[:not(:placeholder-shown)]:text-gray-500 "
               >
                 Confirm Password
@@ -184,7 +199,7 @@ function Page() {
             </div> */}
           </div>
           <div className="text-center text-sm">
-            <span>Already a Memember </span>
+            <span>Already have an account? </span>
             <Link href="/login">
               <span href="#" className=" hover:text-primary">
                 Login
