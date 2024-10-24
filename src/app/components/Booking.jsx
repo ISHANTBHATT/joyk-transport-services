dbb4436685e8b374319be8b9936cc8a05ceab18a
Ishant Bhatt <ishantbhatt1654@gmail.com>
2024-10-24 15:04:22 +0530

    changes: test

--- src/app/components/Booking.jsx
+++ src/app/components/Booking.jsx
@@ -523,13 +523,8 @@ function Booking({ bookingData, setBookingData }) {
         <div className="relative w-full lg:w-auto">
           <p className="text-gray-500 text-sm ml-4">{t.Booking.time}</p>
           <input
-            // className="custom-time-input appearance-none rounded-lg w-full py-2 px-3 text-gray-400 leading-tight border lg:border-none focus:outline-none focus:ring-1 focus:ring-orange-500 focus:border-orange-500"
-            className="custom-time-input block w-full px-3 py-2 text-gray-400 border lg:border-none rounded-md focus:outline-none focus:ring-1 focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
-            // style={{
-            //   appearance: "none",
-            //   WebkitAppearance: "none",
-            //   MozAppearance: "textfield",
-            // }}
+            className="custom-time-input appearance-none rounded-lg w-full py-2 px-3 text-gray-400 leading-tight border lg:border-none focus:outline-none focus:ring-1 focus:ring-orange-500 focus:border-orange-500"
+            // className="custom-time-input block w-full px-3 py-2 text-gray-400 border lg:border-none rounded-md focus:outline-none focus:ring-1 focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
             id="time"
             type="time"
             name="time"
